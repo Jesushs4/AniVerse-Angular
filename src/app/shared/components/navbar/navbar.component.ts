48bcc1de8a289b48a9cb86f87a85d31a551feb7b
Jesushs4 <lagt5799@gmail.com>
2023-12-01 23:52:55 +0100

    CSS changes

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { MenuController } from '@ionic/angular';
 import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
@@ -11,25 +12,27 @@ export class NavbarComponent  implements OnInit {
 
   constructor(
     private router:Router,
-    private auth:AuthService
+    private auth:AuthService,
+    private menu:MenuController
   ) { }
 
   ngOnInit() {}
 
 
   goSearch() {
+    this.menu.close();
     this.router.navigate(['/search'])
   }
   
 
   goLibrary() {
+    this.menu.close();
     this.router.navigate(['/library'])
   }
 
   logout() {
     this.auth.logout();
     this.router.navigate(['/login'])
-    console.log(this.auth.isLogged$);
   }
 
 }
